2f3432020641a85199a5cd1f4707750d70d1d67a
rholibobo <rholipop@gmail.com>
2023-03-01 14:44:51 +0100

    Alivewithplant

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3500;
 
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use("/public/imgs", express.static("./public/imgs"));
+app.use(connectLiveReload());
 
 app.get("/", (req, res) => {
   res.render("home", { pagename: "home" });
