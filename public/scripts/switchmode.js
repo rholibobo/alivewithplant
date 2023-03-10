dbe3f389b44965b4aae7a02f3d1e6aafc12eef79
rholibobo <rholipop@gmail.com>
2023-03-10 12:10:37 +0100

    alivewithplant

--- public/scripts/switchmode.js
+++ public/scripts/switchmode.js
@@ -2,6 +2,7 @@ const buttonDiv = document.getElementById("btn-above");
 const photosBtn = document.getElementById("photos-btn");
 const videosBtn = document.getElementById("videos-btn");
 const wholeImg = document.getElementById("pix");
+const wholeVid = document.getElementById("vids");
 const photosContainer = document.getElementsByClassName("gallery-container");
 const videosContainer = document.getElementsByClassName("video-gallery");
 
@@ -13,13 +14,18 @@ i++
     photosBtn.addEventListener("click", ()=>{
         videosBtn.classList.remove("done");
         photosBtn.classList.add("do");
+        photosBtn.style.animation = "shift 1s ease-in-out infinite";
+        pix.classList.add("flip")
         pix.style.display = 'block';
         videosContainer[i].style.display = "none";
+        vids.classList.remove("flip");
     })
     videosBtn.addEventListener("click", ()=>{
         videosBtn.classList.add("done");
         photosBtn.classList.remove("do");
+        photosBtn.style.animation = "none"
         pix.style.display = "none";
         videosContainer[i].style.display = "flex";
+        vids.classList.add("flip");
     })
 }
